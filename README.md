aa1e01e77c6a4c5294b9addaccaa13bd336bc9a0
Landon Taylor <56940292+landonjefftaylor@users.noreply.github.com>
2023-04-12 12:48:42 -0600

    combine

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# combined_ragtimer
+![RAGTIMER Logo](logo/logo_sm.png)
+
+# RAGTIMER
+Random Assume Guarantee Testing Induced Model Executions for Reachability (RAGTIMER)
+
+## Installation Instructions
+
+The following instructions should guide installation of RAGTIMER and its dependencies. Better instructions are coming soon.
+
+1. Install Microsoft's version of IVy, with instructions for Debian 11 available at https://www.landonjtaylor.net/ivy
+
+2. Install PRISM following instructions at https://prismmodelchecker.org
+
+3. Modify Makefile in the RAGTIMER scripts directory to match your existing PRISM installation folder (not the prism/prism folder, simply the main prism directory)
+
+4. Clone the RAGTIMER repository and play around. The model should be located at scripts/model.sm scripts/model.csl scripts/model.ragtimer as shown by existing files. 
+
+
+## IVy Model Generator
+### Input File Format
+
+All horizontal whitespace is a tab character.
+All vertical whitespace is a newline character.
+
+Example for the Donovan Yeast Polarization Model:
+
+```
+r     l    rl   g    ga   gbg  gd
+50    2    0    50   0    0    0
+-1    -1   -1   -1   -1   50   -1
+r1    0    >    r    0.0038
+r2    r    >    0    0.0004
+r3    r    l    >    rl   l    0.042
+r4    rl   >    r    0.010
+r5    rl   g    >    ga   gbg  0.011
+r6    ga   >    gd   0.100
+r7    gbg  gd   >    g    1050
+r8    0    >    rl   3.21
+```
+
+Line 1: Tab-separated list of chemical names
+
+Line 2: ORDERED initial conditions for each chemical
+
+Line 3: ORDERED target conditions, with -1 meaning "don't care"
+
+Line 4 to end: List of reactions in the following format:
+
+```
+name    (tab-sep reactants)    >    (tab-sep products)    rate constant
+```
+
+All reactions and chemicals must have unique names. Names must include
+no whitespace characters. Use the number `0` to indicate a null reactant or product (i.e. spontaneous production or consumption). Catalysts can be included as normal; their behavior is accounted for in the dependency graph builder.
+
+### IVy Models
+Two IVy Models are generated based off of the information in the input file. The first is written to the file 'test_v2.ivy' and testing of this model only produces one trace before intentionally using assertion failure to stop the tests. The second is written to the file 'test_v3.ivy' and produces the number of traces that is specified by the user. 
+
+## Dependency Graph
+Dependency graph processing documentation can be found at [docs/dependency.md](docs/dependency.md)
+
+## Running RAGTIMER
+To run RAGTIMER navigate to the scripts folder and give the command:
+```sh
+ $ python3 main.py <number of traces wanted>
+```
+RAGTIMER uses the 'model.ragtimer' file found in this repository to generate results. To change the model please modify the 'model.ragtimer' file directly.
+
+By default, RAGTIMER only produces the shortest traces to a specified target. If it is preferred that not only the shortest traces are produced, i.e. there are a limited number of shortest traces available, then add the "loose" parameter when running RAGTIMER. This produces traces that consider all the reactions available, but gives priority to the reactions needed to reach the target. 
+```sh
+ $ python3 main.py <number of traces wanted> loose
+```
+
+To produce the specified number of traces for each of the model's available prefixes add the "each" paramter when running RAGTIMER. 
+```sh
+ $ python3 main.py <number of traces wanted> each
+```
---
+++ ragtimer.py
@@ -0,0 +1,83 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    y = "y"
+    print("Welcome to RAGTIMER. At present, this is two tools joined together. We are working on joining them together more seamlessly but appreciate your patience with this prototype tool.")
+    wantCommute = False
+    if len(sys.argv) == 1:
+        wantCommute = y in input("Would you like to commute? y/n ").lower()
+        print("Thank you. Next time, you can use the option --commute from the command line.")
+    else:
+        wantCommute = "--commute" in sys.argv
+    print("I am assuming you have placed your model in the following files:")
+    print("model.ragtimer (in the RAGTIMER format; see README)")
+    print("model.prop (file with property without time constraints in first line, i.e., x=10 )")
+    print("model.sm (prism model file)")
+    print("model.csl (prism csl property)")
+
+    if wantCommute:
+        with open("options.txt", "w") as options:
+            options.write("model ../model.sm")
+            options.write("\n")
+            options.write("trace ../commute_traces.txt")
+            options.write("\n")
+            property = ""
+            with open("../model.prop", "r") as prop:
+                for line in prop:
+                    property = line.strip()
+                    break
+            options.write("property " + property)
+            options.write("\n")
+            bound = input("Would you prefer to use a time bound (t), recursion bound (r), or both (b)? t/r/b ").lower()
+            if "b" in bound:
+                options.write("recursionBound " + input("What is your max recursion bound (20 is normal)? "))
+                options.write("\n")
+                options.write("timeBound " + input("What is your model property time bound? "))
+                options.write("\n")
+                options.write("flexibility " + input("What time bound flexbility (0.1-0.3 gives good results)? "))
+                options.write("\n")
+                options.write("terminate both")
+                options.write("\n")
+            elif "t" in bound:
+                options.write("recursionBound 100000")
+                options.write("\n")
+                options.write("timeBound " + input("What is your model property time bound? "))
+                options.write("\n")
+                options.write("flexibility " + input("What time bound flexbility (0.1-0.3 gives good results)? "))
+                options.write("\n")
+                options.write("terminate time")
+                options.write("\n")
+            else:
+                options.write("recursionBound " + input("What is your max recursion bound (20 is normal)? "))
+                options.write("\n")
+                options.write("timeBound 100000")
+                options.write("\n")
+                options.write("flexbility 0.0")
+                options.write("\n")
+                options.write("terminate depth")
+                options.write("\n")
+            numCycles = int(input("What max cycle length would you like (0 for no cycles)? "))
+            options.write("cycleLength " + str(numCycles))
+            options.write("\n")
+            options.write("export " + input("Would you like to export to prism, storm, or both? prism/storm/both "))
+            options.write("\n")
+            options.write("verbose false")
+            options.write("\n")
+
+
+    print("Starting RAGTIMER")
+    os.chdir("_ragtimer")
+    loose = y in input("Would you like to add the loose command to RAGTIMER? y/n ").lower()
+    each = y in input("Would you like to add the each command to RAGTIMER? y/n ").lower()
+    qty = int(input("How many traces to generate?"))
+    os.system("python3 main.py ")
+
+    if wantCommute:
+        print("Starting Commuting")
+        os.chdir("../_commute")
+        os.system("make")
+        os.system("make test")
+        print("Look for output files _commute/prism.* or _commute/storm.*")
+        print("You can use these files in your favorite model checker.")
+
