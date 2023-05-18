17bf0460813f52e98f83cd996699387ded04b368
Landon Taylor <landonjefftaylor@gmail.com>
2023-05-18 12:46:03 -0600

    second round

--- results/commute/12react/q100_r2_c0_/benchmark.py
+++ results/commute/12react/q100_r2_c0_/benchmark.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 modelArr = ["12react", "6react", "8react", "2react"]
 looseArr = ["", "loose"]
-qtyArr = ["1", "10", "100", "1000", "10000"]
+qtyArr = ["1", "10", "100", "1000"]
 cycleArr = ["0", "2", "4"]
-recBoundArr = ["2", "10", "20", "40", "80"]
+recBoundArr = ["2", "10", "20"]
 
 #modelArr = ["12react"]
 #looseArr = [""]
@@ -15,44 +15,44 @@ recBoundArr = ["2", "10", "20", "40", "80"]
 #recBoundArr = ["2"]
 
 # # Not commuting, just trace generation
-for model in modelArr:
-    os.system("mkdir results/ragtimer/" + model)
-    for loose in looseArr:
-        for qty in qtyArr:
-            print()
-            print()
-            print(80*"*")
-            print(80*"*")
-            print(80*"*")
+#for model in modelArr:
+#    os.system("mkdir results/ragtimer/" + model)
+#    for loose in looseArr:
+#        for qty in qtyArr:
+#            print()
+#            print()
+#            print(80*"*")
+#            print(80*"*")
+#            print(80*"*")
 
             # copy over the models
-            print("cp models/" + model + "/" + model + ".* .")
-            os.system("cp models/" + model + "/" + model + ".* .")
-            print("mv " + model + ".sm model.sm")
-            os.system("mv " + model + ".sm model.sm")
-            print("mv " + model + ".csl model.csl")
-            os.system("mv " + model + ".csl model.csl")
-            print("mv " + model + ".prop model.prop")
-            os.system("mv " + model + ".prop model.prop")
-            print("mv " + model + ".ragtimer model.ragtimer")
-            os.system("mv " + model + ".ragtimer model.ragtimer")
+#            print("cp models/" + model + "/" + model + ".* .")
+#            os.system("cp models/" + model + "/" + model + ".* .")
+#            print("mv " + model + ".sm model.sm")
+#            os.system("mv " + model + ".sm model.sm")
+#            print("mv " + model + ".csl model.csl")
+#            os.system("mv " + model + ".csl model.csl")
+#            print("mv " + model + ".prop model.prop")
+#            os.system("mv " + model + ".prop model.prop")
+#            print("mv " + model + ".ragtimer model.ragtimer")
+#            os.system("mv " + model + ".ragtimer model.ragtimer")
             
             # run ragtimer (incl. getting time)
             # with open("ragtimer_output.txt", "w") as rto:
-            command = ["/usr/bin/time", "-o", "ragtimer_time.txt", "python3", "ragtimer.py", loose, "qty", qty, "1>", "ragtimer_output.txt", "2>", "/dev/null"]
-            print("running " + " ".join(command))
+#            command = ["/usr/bin/time", "-o", "ragtimer_time.txt", "python3", "ragtimer.py", loose, "qty", qty, "1>", "ragtimer_output.txt", "2>", "/dev/null"]
+#            print("running " + " ".join(command))
             # subprocess.run(command, stdout=rto, stderr=subprocess.DEVNULL)
-            os.system(" ".join(command))
+#            os.system(" ".join(command))
 
             # copy models and results into results folder
-            folder = "results/ragtimer/" + model + "/" + qty + "_" + loose
-            os.system("mkdir " + folder)
-            os.system("cp model.* " + folder)
-            os.system("ls *.txt")
-            os.system("cp *.txt " + folder)
-            os.system("cp *.py " + folder)
-            with open(folder + "/timestamp.txt", "w") as f:
-                f.write(str(datetime.now()))
+#            folder = "results/ragtimer/" + model + "/" + qty + "_" + loose
+#            os.system("mkdir " + folder)
+#            os.system("cp model.* " + folder)
+#            os.system("ls *.txt")
+#            os.system("cp *.txt " + folder)
+#            os.system("cp *.py " + folder)
+#            with open(folder + "/timestamp.txt", "w") as f:
+#                f.write(str(datetime.now()))
 
 
 # Not commuting, just trace generation
